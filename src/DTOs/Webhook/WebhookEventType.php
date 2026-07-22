4f16c0cef29dfefca276e9f3665bc6b1631a24a0
bayurifkialghifari <bayurifkialgh@gmail.com>
2026-07-22 14:58:57 +0700

    update webhook event type

---
+++ src/DTOs/Webhook/WebhookEventType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Bayurifkialghifari\WaxumApi\DTOs\Webhook;
+
+enum WebhookEventType: string
+{
+    case ALL = 'all';
+    case MESSAGE = 'message';
+    case RECEIPT = 'receipt';
+    case PRESENCE = 'presence';
+    case CHAT_PRESENCE = 'chat_presence';
+    case GROUP_UPDATE = 'group_update';
+    case JOINED_GROUP = 'joined_group';
+    case QR_CODE = 'qr_code';
+    case PAIR_CODE = 'pair_code';
+    case CONNECTED = 'connected';
+    case DISCONNECTED = 'disconnected';
+    case LOGGED_OUT = 'logged_out';
+    case PICTURE_UPDATE = 'picture_update';
+    case USER_ABOUT_UPDATE = 'user_about_update';
+    case PUSH_NAME_UPDATE = 'push_name_update';
+    case CONTACT_UPDATE = 'contact_update';
+    case DEVICE_LIST_UPDATE = 'device_list_update';
+    case PIN_UPDATE = 'pin_update';
+    case MUTE_UPDATE = 'mute_update';
+    case ARCHIVE_UPDATE = 'archive_update';
+    case MARK_CHAT_AS_READ = 'mark_chat_as_read';
+    case UNDECRYPTABLE_MESSAGE = 'undecryptable_message';
+    case CLIENT_OUTDATED = 'client_outdated';
+    case OFFLINE_SYNC_PREVIEW = 'offline_sync_preview';
+    case OFFLINE_SYNC_COMPLETED = 'offline_sync_completed';
+    case SCHEDULED_SENT = 'scheduled_sent';
+    case SCHEDULED_FAILED = 'scheduled_failed';
+    case BLAST_PROGRESS = 'blast_progress';
+    case BLAST_COMPLETED = 'blast_completed';
+    case KEEP_ALIVE_TIMEOUT = 'keep_alive_timeout';
+
+    /**
+     * @return string[]
+     */
+    public static function values(): array
+    {
+        return array_column(self::cases(), 'value');
+    }
+}
